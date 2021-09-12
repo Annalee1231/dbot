28363f5a0d292823d8b3b6ff577464615afb6c34
Annalee1231 <90571068+Annalee1231@users.noreply.github.com>
2021-09-13 03:23:37 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,23 @@
+import asyncio
 import discord
-from discord.ext import commands
-import os
 
-client = commands.Bot(command_prefix = '!')
+app = discord.Client()
 
-@client.event
+token = "your_token"
+
+@app.event
 async def on_ready():
+    print("다음으로 로그인합니다 : ")
+    print(app.user.name)
+    print(app.user.id)
+    print("==========")
+    await app.change_presence(game=discord.Game(name="안녕하세요 :)", type=1))
 
-  # [discord.Status.online = 온라인],[discord.Status.idle = 자리비움],[discord.Status.dnd = 다른용무],[discord.Status.offline = 오프라인]
-  await client.change_presence(status=discord.Status.online)
+@app.event
+async def on_message(message):
+    if message.author.bot:
+        return None
+    if message.content == "!인증":
+        await app.send_message(message.channel,"https://discord.com/channels/831033031648739368/886107201712820275/886107863574011924")
 
-  await client.change_presence(activity=discord.Game(name="1.2.65 Reminisce"))
-  #await client.change_presence(activity=discord.Streaming(name="스트림 방송중", url='링크'))
-  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="노래 듣는중"))
-  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="영상 시청중"))
-  
-  print("봇 이름:",client.user.name,"봇 아이디:",client.user.id,"봇 버전:",discord.__version__)
-
-
-client.run(os.environ['ODg2NTIyMDI5OTc1Njc0OTIx.YT2z-g.mRw88Z_q3c72hRnKkqR8kUIicwk'])
+app.run('ODg2NTIyMDI5OTc1Njc0OTIx.YT2z-g.bPwg0CTXUs2wdt51SlYaviM5yzI')
